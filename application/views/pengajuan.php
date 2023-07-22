61e14bb2c2da0e947936ca6cc8ab67a026f75e5e
gtpesanten22 <cakromat26@gmail.com>
2023-07-22 09:25:44 +0700

    update pengajuan

--- application/views/pengajuan.php
+++ application/views/pengajuan.php
@@ -18,20 +18,19 @@
                 <div class="box">
                     <div class="box-header">
                         <?php if ($this->session->flashdata('ok')) : ?>
-                        <div class="alert alert-success alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <i class="fa fa-check"></i> <?= $this->session->flashdata('ok') ?>
-                        </div>
+                            <div class="alert alert-success alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <i class="fa fa-check"></i> <?= $this->session->flashdata('ok') ?>
+                            </div>
                         <?php endif; ?>
                         <?php if ($this->session->flashdata('error')) : ?>
-                        <div class="alert alert-danger alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <i class="fa fa-check"></i> <?= $this->session->flashdata('error') ?>
-                        </div>
+                            <div class="alert alert-danger alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <i class="fa fa-check"></i> <?= $this->session->flashdata('error') ?>
+                            </div>
                         <?php endif; ?>
                         <h3 class="box-title">Data Pengajuan</h3>
-                        <a href="<?= base_url('pengajuan/add'); ?>" class="btn btn-sm btn-success pull-right"><i
-                                class="fa fa-plus-circle"></i> Tambah
+                        <a href="<?= base_url('pengajuan/add'); ?>" class="btn btn-sm btn-success pull-right"><i class="fa fa-plus-circle"></i> Tambah
                             Data</a>
                     </div><!-- /.box-header -->
                     <div class="box-body">
@@ -46,6 +45,7 @@
                                         <th>Daerah</th>
                                         <th>Nominal</th>
                                         <th>Status</th>
+                                        <th>Tanggal</th>
                                         <th>#</th>
                                     </tr>
                                 </thead>
@@ -53,34 +53,30 @@
                                     <?php
                                     $no = 1;
                                     foreach ($data as $dt) : ?>
-                                    <tr>
-                                        <td><?= $no++; ?></td>
-                                        <td><?= $dt->kode_pengajuan; ?></td>
-                                        <td><?= $dt->nama; ?></td>
-                                        <td><?= $dt->kriteria; ?></td>
-                                        <td><?= $dt->daerah; ?></td>
-                                        <td><?= rupiah($dt->nom_kriteria + $dt->transport + $dt->sopir); ?></td>
-                                        <td>
-                                            <?= $dt->status === 'belum' ? "<span class='badge bg-red'>Belum</span>" : "" ?>
-                                            <?= $dt->status === 'proses' ? "<span class='badge bg-yellow'>Proses</span>" : "" ?>
-                                            <?= $dt->status === 'ditolak' ? "<span class='badge bg-red'>Ditolak</span>" : "" ?>
-                                            <?= $dt->status === 'disetujui' ? "<span class='badge bg-green'>Disetujui</span>" : "" ?>
-                                            <?= $dt->status === 'selesai' ? "<span class='badge bg-blue'>Selesai</span>" : "" ?>
-                                        </td>
-                                        <td>
-                                            <?php if ($dt->status === 'belum' || $dt->status === 'ditolak') { ?>
-                                            <a href="<?= base_url('pengajuan/ajukan/' . $dt->kode_pengajuan); ?>"
-                                                onclick="return confirm('Yakin akan diajukan ?')"
-                                                class="btn btn-success btn-xs">Ajukan</a> |
-                                            <a href="<?= base_url('pengajuan/edit/' . $dt->kode_pengajuan) ?>"
-                                                class="btn btn-warning btn-xs">Edit</a>
-                                            <a href="<?= base_url('pengajuan/del/' . $dt->kode_pengajuan); ?>"
-                                                onclick="return confirm('Yakin akan dihpaus ?')"
-                                                class="btn btn-danger btn-xs">Hapus</a>
-                                            <?php }  ?>
+                                        <tr>
+                                            <td><?= $no++; ?></td>
+                                            <td><?= $dt->kode_pengajuan; ?></td>
+                                            <td><?= $dt->nama; ?></td>
+                                            <td><?= $dt->kriteria; ?></td>
+                                            <td><?= $dt->daerah; ?></td>
+                                            <td><?= rupiah($dt->nom_kriteria + $dt->transport + $dt->sopir); ?></td>
+                                            <td><?= $dt->tgl_jalan; ?></td>
+                                            <td>
+                                                <?= $dt->status === 'belum' ? "<span class='badge bg-red'>Belum</span>" : "" ?>
+                                                <?= $dt->status === 'proses' ? "<span class='badge bg-yellow'>Proses</span>" : "" ?>
+                                                <?= $dt->status === 'ditolak' ? "<span class='badge bg-red'>Ditolak</span>" : "" ?>
+                                                <?= $dt->status === 'disetujui' ? "<span class='badge bg-green'>Disetujui</span>" : "" ?>
+                                                <?= $dt->status === 'selesai' ? "<span class='badge bg-blue'>Selesai</span>" : "" ?>
+                                            </td>
+                                            <td>
+                                                <?php if ($dt->status === 'belum' || $dt->status === 'ditolak') { ?>
+                                                    <a href="<?= base_url('pengajuan/ajukan/' . $dt->kode_pengajuan); ?>" onclick="return confirm('Yakin akan diajukan ?')" class="btn btn-success btn-xs">Ajukan</a> |
+                                                    <a href="<?= base_url('pengajuan/edit/' . $dt->kode_pengajuan) ?>" class="btn btn-warning btn-xs">Edit</a>
+                                                    <a href="<?= base_url('pengajuan/del/' . $dt->kode_pengajuan); ?>" onclick="return confirm('Yakin akan dihpaus ?')" class="btn btn-danger btn-xs">Hapus</a>
+                                                <?php }  ?>
 
-                                        </td>
-                                    </tr>
+                                            </td>
+                                        </tr>
                                     <?php endforeach; ?>
                                 </tbody>
                             </table>
